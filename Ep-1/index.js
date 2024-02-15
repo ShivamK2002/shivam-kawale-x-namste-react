8e71745a2de3ead3a86db3c041d6ddb8f972538f
Shivam Kawale <ultimateengineer2002@gmail.com>
2024-02-15 11:56:28 +0530

    ep-01

---
+++ Ep-1/index.js
@@ -0,0 +1,7 @@
+const heading = React.createElement(
+  "a",
+  { id: "heading", href: "google.com" },
+  "Shivam Kawale"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
